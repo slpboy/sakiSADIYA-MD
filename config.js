03b4a53fa7d7059e057df208f99a36f35da5fe8f
slpboy <binancesenura@gmail.com>
2025-05-07 05:04:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //Put Session-id Here
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "", //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "", //Put Your Github Auth Token
+SESSION_ID: process.env.SESSION_ID || "SADIYA-MD=GVFGRbqK#M3nDVAoZ76xBhRgOFvzyXXpn2DLcR-0Xy7mYkGvrp6s", //Put Session-id Here
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "slpboy", //Put Your Gitub Username
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "tL4BUyKjuW1V8DMofSGZw58iOeCCWI1GvFyg", //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || "SADIYA-MD-DATABASE", //Bot Database Repo Name
 SESSION_NAME: process.env.SESSION_NAME || "session"
 };
